c5b845821db7c7174a3e7eba279888831a737808
Noxer <tim.scheuermann@gmail.com>
2012-04-29 21:07:52 +0200

    Added functions for library support

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-orgASM --working-directory kernel/ --input-file kernel/core.dasm16 --output-file AtlasOS.bin 
-pause
+@orgASM --working-directory kernel/ --input-file kernel/core.dasm16 --output-file AtlasOS.bin
+@pause
