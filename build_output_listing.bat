476142ec268fe9a4aa2807c9edf097f31fbbe97a
chessmaster42 <chessmaster42@gmail.com>
2012-04-30 19:35:42 -0500

    Updated assembler to latest version (was renamed)

--- build_output_listing.bat
+++ build_output_listing.bat
@@ -1,2 +1,2 @@
-@orgASM --working-directory kernel/ --input-file kernel/core.dasm16  --output-file AtlasOS.bin --listing Atlas_listing.txt
+@organic --working-directory kernel/ --input-file kernel/core.dasm16  --output-file AtlasOS.bin --listing Atlas_listing.txt
 @pause
--- build_upload.bat
+++ build_upload.bat
@@ -1,2 +1,2 @@
-@orgASM --working-directory kernel/ --input-file kernel/core.dasm16 --output-file AtlasOS.bin --0x10co.de
+@organic --working-directory kernel/ --input-file kernel/core.dasm16 --output-file AtlasOS.bin --0x10co.de
 @pause
