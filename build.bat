2e25e519b04bd0bafca844d01e06f79249d82589
bungao <roliver8143@gmail.com>
2012-06-17 23:50:21 +1000

    Changed build organisation, changed to ascii linefeed symbol, AltasText enhancements
    - included 0x10c-devkit solution file.
    - created a "make" file for organic and dcpu-toolchain assembler
    - AtlasText now can edit a 1024 character string in memory
    - ability to delete back through the scrolled text

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-@organic --working-directory kernel/ --input-file kernel/core.dasm16 --output-file AtlasOS.bin --plain-text atlas.txt --listing Atlas_listing.txt
+@organic --working-directory kernel/ --input-file kernel/make.dasm16 --output-file AtlasOS.bin --plain-text atlas.txt --listing Atlas_listing.txt
 @pause
