ee206f727e7e10f971c0ca97c3d57e011c4493b6
Tim Scheuermann <tim.scheuermann@gmail.com>
2012-06-27 10:59:18 +0200

    Fixed building for linux

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mono organic.exe --working-directory kernel/ --input-file kernel/core.dasm16 --output-file AtlasOS.bin
+mono Organic.exe --working-directory kernel/ --input-file kernel/core.dasm16 --output-file AtlasOS.bin
